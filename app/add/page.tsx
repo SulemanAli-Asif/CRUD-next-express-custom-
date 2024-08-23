7e6be2b9deb51e2b174a524f1c84ee0578c68b3e
suleman ali <suleman.ali.asif@outlook.com>
2024-08-24 01:32:39 +0500

    removed all previous auth from the projetc

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,13 +1,10 @@
 import AddForm from "@/components/AddForm";
-import ProtectedLayout from "@/components/ProtectedLayout";
 import React from "react";
 
 function Add() {
   return (
     <div>
-      <ProtectedLayout>
-        <AddForm />
-      </ProtectedLayout>
+      <AddForm />
     </div>
   );
 }
--- app/update/[id]/page.tsx
+++ app/update/[id]/page.tsx
@@ -1,13 +1,11 @@
-import ProtectedLayout from "@/components/ProtectedLayout";
+// import ProtectedLayout from "@/components/ProtectedLayout";
 import UpdateForm from "@/components/UpdateForm";
 import React from "react";
 
 function Update() {
   return (
     <div>
-      <ProtectedLayout>
-        <UpdateForm />
-      </ProtectedLayout>
+      <UpdateForm />
     </div>
   );
 }
